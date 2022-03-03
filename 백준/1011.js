63c1f74fa99ca037320b9789ffee9db3f3933c2b
Sumin Park <clappingmin@gmail.com>
2022-03-04 01:14:58 +0900

    2022.03.03

--- 백준/1011.js
+++ 백준/1011.js
@@ -11,27 +11,27 @@ while (t--) {
   let end = Number(data[1]);
 
   const dist = end - start; // 이동해야 할 거리
-  let x = 0; // 작동횟수
-  let result = 1; // 결과
+  let x = 0;
+  let cnt = 1; // 작동횟수
 
   while (x < dist) {
-    // 작동횟수 < 이동해야 할 거리
-    result++;
-    x += Math.floor(result / 2);
+    // 이동할 수 있는 거리(이동거리) < 이동해야 할 거리
+    cnt++;
+    x += Math.floor(cnt / 2);
   }
 
-  console.log(result - 1);
+  console.log(cnt - 1);
 
   idx++;
 }
 /*
-result = 2;
+cnt = 2;
 1번 작동에 갈 수 있는 거리 x = (0) + 1;
 
-result = 3;
+cnt = 3;
 2번 작동에 갈 수 있는 거리 x =  (1) + 1;
 
-result = 4;
+cnt = 4;
 3번 작동에 갈 수 있는 거리 x = (1 + 1) + 2;
 
 */
